633f3fc340358c64cf76de819c67195c3b03e373
Windi Octavia <windioctav@gmail.com>
2025-11-12 18:43:58 +0700

    feat: Initial project setup with Laravel 12

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PageController::class, 'home']);
+Route::get('/about', [PageController::class, 'about']);
+Route::get('/services', [PageController::class, 'services']);
+Route::get('/team', [PageController::class, 'team']);
+Route::get('/contact', [PageController::class, 'contact']);
